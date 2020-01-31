fb5f6ed65fb619bc0e36b6f4211eab68cfb343b4
Jubayed <$hahNewaz1Jub$hant1>
2020-01-31 02:34:10 +0100

    Role based security added

---
+++ src/main/java/com/example/SpringBootSecurity/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.SpringBootSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("shanto").password(passwordEncoder().encode("shanto")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager")).roles("MANAGER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/manager/**").hasAnyRole("ADMIN","MANAGER")
+                .antMatchers("/api/public/**").authenticated()
+                .and()
+                .httpBasic();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
